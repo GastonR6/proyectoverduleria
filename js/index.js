84b05e0a020de95476139daad5a1b3c730055306
GastonR6 <gastonrodriguez2354@hotmail.com>
2021-10-02 19:50:38 -0300

    Actualización

--- js/index.js
+++ js/index.js
@@ -7,3 +7,18 @@ $(document).ready(function () {
         });
     }
 });
+
+/* código contador */
+
+$(document).ready(function(){
+    $('.count').prop('disabled', true);
+       $(document).on('click','.plus',function(){
+        $('.count').val(parseInt($('.count').val()) + 1 );
+    });
+    $(document).on('click','.minus',function(){
+        $('.count').val(parseInt($('.count').val()) - 1 );
+            if ($('.count').val() == 0) {
+                $('.count').val(1);
+            }
+        });
+ });
